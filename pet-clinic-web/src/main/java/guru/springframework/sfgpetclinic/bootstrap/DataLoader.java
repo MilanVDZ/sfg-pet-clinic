9cb0f2d4136555434f1425950ebe5ea0c59064ce
MilanVDZ <Milan.VanderZypen@student.intecbrussel.be>
2021-10-25 05:36:21 +0200

    Refactored Map to abstract ID Generation. Closes #23

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,46 +22,28 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
         ownerService.save(owner2);
 
         System.out.println("*Loaded Owners*");
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Milan");
         vet1.setLastName("Van der Zypen");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Dirk");
         vet2.setLastName("Verbrugge");
-
         vetService.save(vet2);
 
         System.out.println("*Loaded Vets*");
-
-
-
-
-
-
-
-
-
-
     }
 }
